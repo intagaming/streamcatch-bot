2834b482bc8c90a792e0bdd4397a6e3a22f4772a
An Hoang <xuanan2001@pm.me>
2024-08-28 17:27:44 +0700

    wip

--- broadcaster/agent_test.go
+++ broadcaster/agent_test.go
@@ -1,7 +1,10 @@
 package broadcaster
 
 import (
+	"context"
+	"github.com/stretchr/testify/assert"
 	"go.uber.org/zap/zaptest"
+	"io"
 	"testing"
 	"time"
 )
@@ -11,12 +14,69 @@ type testListener struct{}
 func (tl *testListener) Status(stream *Stream, status StreamStatus) {}
 func (tl *testListener) Close(stream *Stream, reason error)         {}
 
+type testFfmpegCmder struct {
+	stdin io.Reader
+}
+
+func (t *testFfmpegCmder) SetStdin(pipe io.Reader) {
+	t.stdin = pipe
+}
+
+func (t *testFfmpegCmder) SetStderr(pipe io.Writer) {
+}
+
+func (t *testFfmpegCmder) Start() error {
+	return nil
+}
+
+func (t *testFfmpegCmder) Wait() error {
+	return nil
+}
+
+type testDummyStreamFfmpegCmder struct {
+	stdout io.Writer
+}
+
+func (t *testDummyStreamFfmpegCmder) SetStdout(pipe io.Writer) {
+	t.stdout = pipe
+}
+
+func (t *testDummyStreamFfmpegCmder) SetStderr(pipe io.Writer) {
+}
+
+func (t *testDummyStreamFfmpegCmder) Start() error {
+	return nil
+}
+
+func (t *testDummyStreamFfmpegCmder) Wait() error {
+	// TODO: wait for context
+}
+
 func TestAgent_DummyStream(t *testing.T) {
 	logger := zaptest.NewLogger(t)
-	broadcaster := New(logger.Sugar(), &Config{})
+
+	ffmpegCmder := &testFfmpegCmder{}
+	dummyFfmpegCmder := &testDummyStreamFfmpegCmder{}
+	broadcaster := New(logger.Sugar(), &Config{
+		FfmpegCmderCreator: func(ctx context.Context, config *Config, streamId int64) FfmpegCmder {
+			return ffmpegCmder
+		},
+		DummyStreamFfmpegCmderCreator: func(ctx context.Context, streamUrl string) DummyStreamFfmpegCmder {
+			return dummyFfmpegCmder
+		},
+		StreamAvailableChecker: func(streamId int64) (bool, error) {
+			return false, nil
+		},
+		StreamWaiter: func(agent *Agent) error {
+			return nil
+		},
+		Streamer: func(ctx context.Context, stream *Stream, pipeWrite *io.PipeWriter) error {
+			return nil
+		},
+	})
 	listener := testListener{}
 
-	_ = broadcaster.HandleStream(&Stream{
+	a := broadcaster.HandleStream(&Stream{
 		Id:             1,
 		Url:            "http://TEST_URL",
 		Platform:       "twitch",
@@ -25,5 +85,21 @@ func TestAgent_DummyStream(t *testing.T) {
 		Listener:       &listener,
 	})
 
-	// TODO: expect ffmpeg stream to receive dummy stream outputs
+	assert.Eventually(t, func() bool {
+		return a.streamStarted
+	}, time.Second, 10*time.Millisecond)
+	//
+	//// TODO: expect ffmpeg stream to receive dummy stream outputs
+	//dummyOutData := []byte{1, 2, 3, 4, 5}
+	//_, err := dummyFfmpegCmder.stdout.Write(dummyOutData)
+	//if err != nil {
+	//	t.Fatalf("Failed to write dummy data: %v", err)
+	//}
+	//ffmpegCmderIn := make([]byte, 5)
+	//_, err = ffmpegCmder.stdin.Read(ffmpegCmderIn)
+	//if err != nil {
+	//	t.Fatalf("Failed to read stdin ffmpeg cmder: %v", err)
+	//}
+	//
+	//assert.Equal(t, dummyOutData, ffmpegCmderIn)
 }
--- go.mod
+++ go.mod
@@ -5,13 +5,17 @@ go 1.23.0
 require (
 	github.com/bwmarrin/discordgo v0.28.1
 	github.com/nicklaw5/helix/v2 v2.30.0
+	github.com/stretchr/testify v1.8.1
 	go.uber.org/zap v1.27.0
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang-jwt/jwt/v4 v4.0.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
 	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
